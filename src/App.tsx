17522264f57ceae2aa842d7a65262641190c8844
furkanevin <furkanevin00@gmail.com>
2024-06-26 22:08:34 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Create from "./pages/Create";
+import Detail from "./pages/Detail";
+import Edit from "./pages/Edit";
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { Note, NoteData, Tag } from "./types";
+import { v4 } from "uuid";
+import Layout from "./components/Layout";
+
+const App = () => {
+  const [notes, setNotes] = useLocalStorage<Note[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  // etiket oluşturma fonk
+  const createTag = (tag: Tag): void => {
+    setTags([...tags, tag]);
+  };
+
+  // not oluşturma fonk
+  const createNote = (noteData: NoteData): void => {
+    // formdan gelen veriye id ekle
+    const newNote: Note = {
+      id: v4(),
+      ...noteData,
+    };
+
+    setNotes([...notes, newNote]);
+  };
+
+  // note silme fonk
+  const deleteNote = (id: string): void => {
+    setNotes(notes.filter((n) => n.id !== id));
+  };
+
+  // note düzenleme fonk
+  const updateNote = (id: string, updatedData: NoteData): void => {
+    const updatedArr = notes.map((note) =>
+      note.id === id ? { id, ...updatedData } : note
+    );
+
+    setNotes(updatedArr);
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main notes={notes} availableTags={tags} />} />
+
+        <Route
+          path="/new"
+          element={
+            <Create
+              handleSubmit={createNote}
+              createTag={createTag}
+              availableTags={tags}
+            />
+          }
+        />
+
+        <Route path="/note/:id" element={<Layout notes={notes} />}>
+          <Route index element={<Detail deleteNote={deleteNote} />} />
+
+          <Route
+            path="edit"
+            element={
+              <Edit
+                handleSubmit={updateNote}
+                createTag={createTag}
+                availableTags={tags}
+              />
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
